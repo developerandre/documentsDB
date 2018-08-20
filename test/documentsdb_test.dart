6b48652b5e69e16029c5bb0d44b1bef07cdf37ea
andredeveloper <andredegbe@gmail.com>
2018-08-20 14:09:41 +0000

    bug fixed

--- test/documentsdb_test.dart
+++ test/documentsdb_test.dart
@@ -1,29 +1,27 @@
-import 'dart:async';
 import 'dart:io';
-import 'dart:math';
 
-import 'package:test/test.dart';
+//import 'package:test/test.dart';
 
 import 'package:documentsdb/documentsdb.dart';
 
-void main() {
-  test('adds one to input values', () async {
-    final path = Directory.current.path + '/test/';
-    //File file;
-    /* File file = File(path + 'test.db');
+void main() async {
+  //test('adds one to input values', () async {
+  final path = Directory.current.path + '/test/';
+  //File file;
+  /* File file = File(path + 'test.db');
     if (file.existsSync()) {
       file.deleteSync();
     }
     file = File(path + 'init.db');
     file.copySync(path + 'test.db'); */
 
-    final db =
-        DocumentsDB(path + 'test.db', timestampData: true, inMemoryOnly: false);
-    await db.open();
-    await db.ensureIndex(['type.at'], mandatory: true, unique: true);
-    //await db.removeIndex(['type']);
-    //print(await db.findOneAndRemove({'a': '5'}));
-    /* db.importFromFile([
+  final db =
+      DocumentsDB(path + 'test.db', timestampData: true, inMemoryOnly: false);
+  await db.open();
+  await db.ensureIndex(['type.at'], mandatory: true, unique: true);
+  //await db.removeIndex(['type']);
+  //print(await db.findOneAndRemove({'a': '5'}));
+  /* db.importFromFile([
       {"_id": "5b78bdb6702de39b30e6f68b", "b": "6"},
       {"_id": "5b78bdb6702de39b30e6f68c", "b": "6"},
       {"_id": "5b78bdb6702de39b30e6f68d", "b": "6"},
@@ -33,57 +31,57 @@ void main() {
         "o": {"sqdq": []}
       }
     ]); */
-    await db.insertMany([
-      {
-        'type': {'at': '1'}
-      },
-      {
-        'type': {'at': '2'}
-      },
-      {
-        'type': {'at': '3'}
-      },
-      {
-        'type': {'at': '4'}
-      }
-    ]);
-    /* db.watch().listen((data) {
+  await db.insertMany([
+    {
+      'type': {'at': '1'}
+    },
+    {
+      'type': {'at': '2'}
+    },
+    {
+      'type': {'at': '3'}
+    },
+    {
+      'type': {'at': '4'}
+    }
+  ]);
+  /* db.watch().listen((data) {
       if (data.isNotEmpty) print('without $data');
     });
     db.watch({"a": "1"}).listen((data) {
       if (data.isNotEmpty) print('with query $data');
     }); */
-    /* await db.update({
+  /* await db.update({
     "34": 52
   }, {
     'top': {"keyi": 'beh'}
   }, upsert: true); */
 
-    await db.update({
-      'type.at': '4'
-    }, {
-      Op.unset: {"type.at": true}
-    }, upsert: true);
+  await db.update({
+    'type.at': '4'
+  }, {
+    Op.unset: {"type.at": true}
+  }, upsert: true);
 
-    db.onUpdate.listen((data) {
-      print('on update $data');
-    });
-    db.onInsert.listen((data) {
-      print('on onInsert $data');
-    });
-    db.onRemove.listen((data) {
-      print('on onRemove $data');
-    });
-    print((await db.find(
-      {
-        // Op.exists: {'e.c.d[0]': true, 'a': true}
-      },
-      projection: {'e': false, 'a': false},
-      /*  sort: {'e.c.d[0].dsqd': 1, 'a': 1},
+  db.onUpdate.listen((data) {
+    print('on update $data');
+  });
+  db.onInsert.listen((data) {
+    print('on onInsert $data');
+  });
+  db.onRemove.listen((data) {
+    print('on onRemove $data');
+  });
+  print((await db.find(
+    {
+      // Op.exists: {'e.c.d[0]': true, 'a': true}
+    },
+    projection: {'e': false, 'a': false},
+    /*  sort: {'e.c.d[0].dsqd': 1, 'a': 1},
         skip: 2,
         limit: 1 */
-    )));
-    /* var ids = await db.insertMany([
+  )));
+  /* var ids = await db.insertMany([
       {'a': '1'},
       {'a': '2'},
       {'a': '3'},
@@ -124,8 +122,8 @@ void main() {
       Op.gt: {'a': 0},
     }));
  */
-    //await Future.delayed(Duration(days: 1));
-    await db.tidy();
-    await db.close();
-  });
+  //await Future.delayed(Duration(days: 1));
+  await db.tidy();
+  await db.close();
+  // });
 }
